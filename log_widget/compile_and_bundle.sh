099a319c5b8346475b597a22abe69c97f6dddd95
Brady Bilderback <brady@bilderback.me>
2020-10-22 19:47:42 -0400

    created a script to handle bundling up the widget

---
+++ log_widget/compile_and_bundle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this is really quick and dirty and basically assumes everything it needs to compile is already there
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+rm ./classes/fm/wmtu/*.class
+
+javac -cp .:lib/commons-text-1.9.jar:lib/commons-lang3-3.11.jar:lib/log4j-1.2-api-2.13.3.jar:lib/log4j-api-2.13.3.jar:lib/log4j-core-2.13.3.jar:lib/ras-core-api.jar LoggingWidget.java
+
+mv *.class ./classes/fm/wmtu/
+
+if [ -f LoggingWidget.zip ]; then 
+    rm LoggingWidget.zip 
+fi
+
+zip -rq LoggingWidget.zip classes lib resources plugin.xml
+
+exit 0
